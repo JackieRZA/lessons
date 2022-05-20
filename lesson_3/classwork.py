d3297f741a0f08d41c01878dba8b0c495d2e14d6
JackieRZA <jediminds29@gmail.com>
2022-05-20 21:37:40 +0300

    add classwork.py file

---
+++ lesson_3/classwork.py
@@ -0,0 +1,70 @@
+In [1]: x=17/2*(3+2)
+
+In [2]: x
+Out[2]: 42.5
+
+In [3]: x=(2+17)/2*3
+
+In [4]: x
+Out[4]: 28.5
+
+In [5]: x=(19%4)+(15/2*3)
+
+In [6]: x
+Out[6]: 25.5
+
+In [7]: x=(15+6)-1*4
+
+In [8]: x
+Out[8]: 17
+
+In [9]: x=((15+6)-10)*4
+
+In [10]: x
+Out[10]: 44
+
+In [11]: zhenya=27
+
+In [12]: oleg=28
+
+In [13]: bob=29
+
+In [14]: zhenya+oleg+bob
+Out[14]: 72
+
+In [15]: x=(zhenya+oleg+bob)/3
+
+In [16]: x
+Out[16]: 28.0
+
+In [17]: my_list=[1, 1.0, 2, 2, 5.0, "python", "python3", "python3"]
+
+
+In [3]: my_list = [1, 1.0, 2, 2, 5.0, "python", "python3", "python3"]
+
+In [4]: my_list[3:6][::-1]
+Out[4]: ['python', 5.0, 2]
+
+In [5]: set(my_list)
+Out[5]: {1, 2, 5.0, 'python', 'python3'}
+
+In [6]: my_set=set(my_list)
+
+In [7]: len(my_list)-len(my_set)
+Out[7]: 3
+
+In [8]: my=my_list[3:6]
+
+In [9]: my
+Out[9]: [2, 5.0, 'python']
+
+In [10]: my[::-1]
+Out[10]: ['python', 5.0, 2]
+
+In [11]: size = 3
+
+In [18]: from math import sqrt
+
+
+In [20]: (size*4, size**2, sqrt(size**2*2))
+Out[20]: (12, 9, 4.242640687119285)
