c0cb5a8f0d8d6a5f4dcab0cc342c05eee8f067bc
JackieRZA <105815613+JackieRZA@users.noreply.github.com>
2022-05-24 23:19:35 +0300

    Update solution_02.py

---
+++ lesson_3/solution_02.py
@@ -0,0 +1,13 @@
+In [36]: dep = 2130
+
+In [37]: year1 = dep + dep * 0.1 + 120
+
+In [38]: year2 = year1 + year1 * 0.1 + 120
+
+In [39]: year3 = year2 + year2 * 0.1 + 120
+
+In [40]: year4 = year3 + year3 * 0.1 + 120
+
+In [41]: year5 = year4 + year4 * 0.1 + 120
+
+In [42]: print(year5)
