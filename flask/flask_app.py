89683772ed681dfb821277f4162738fcfed575f2
JackieRZA <jediminds29@gmail.com>
2022-07-12 08:01:19 +0300

    add db

--- flask/flask_app.py
+++ flask/flask_app.py
@@ -1,6 +1,10 @@
 import logging
 
-from flask import Flask, request
+from flask import Flask, request, Response
+from sqlalchemy.orm import sessionmaker
+
+from utils import create_database_if_not_exists, setup_db_engine
+from models import Base, User, Profile, Address, Product, Purchase
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -8,41 +12,33 @@ logger = logging.getLogger(__name__)
 app = Flask(__name__)
 
 
-def dict_to_str(data: dict) -> str:
-    result = []
-    for key, value in data.items():
-        result.append(f"{key}: {value}")
-    return ", ".join(result)
-
-
-def dict_to_str_gen(data: dict) -> str:
-    return ", ".join([
-        f"{key}: {value}" for key, value in data.items()
-    ])
-
-
-@app.route("/", methods=["GET", "POST"])
+@app.route("/", methods=["GET"])
 def index():
+    return Response("Index page")
+
+
+@app.route("/user/", methods=["GET", "POST"])
+def user():
     if request.method == "POST":
-        logger.info(
-            dict_to_str(
-                request.form.to_dict()
-            )
-        )
-        return request.form.to_dict()
+        data = request.json
+        if not data.get("email") or not data.get("password"):
+            return Response(status=400)
 
-    logger.info(
-        dict_to_str_gen(
-            request.args.to_dict()
-        )
-    )
-    return request.args.to_dict()
+        new_user = User(email=data.get("email"), password=data.get("password"))
+        app.current_session.add(new_user)
+        app.current_session.commit()
+        return Response(str(new_user), status=201)
 
-
-@app.route("/test/", methods=["GET"])
-def test():
-    return "Test URL"
+    data = map(str, app.current_session.query(User).all())
+    return Response("\n".join(data))
 
 
 if __name__ == "__main__":
+    engine = setup_db_engine()
+    create_database_if_not_exists(engine=engine)
+
+    Base.metadata.create_all(engine)
+    CurrentSession = sessionmaker(bind=engine)
+    app.current_session = CurrentSession()
+
     app.run()
---
+++ flask/utils.py
@@ -0,0 +1,21 @@
+from sqlalchemy import create_engine
+from sqlalchemy.engine import Engine
+from sqlalchemy_utils import create_database, database_exists
+
+DB_USER = "jackie"
+DB_PASSWORD = "jackie771"
+DB_NAME = "jackie"
+DB_ECHO = True
+
+
+def setup_db_engine() -> Engine:
+    return create_engine(
+        f"postgresql://{DB_USER}:{DB_PASSWORD}@localhost/{DB_NAME}", echo=True,
+    )
+
+
+def create_database_if_not_exists(engine: Engine) -> bool:
+    if not database_exists(engine.url):
+        create_database(engine.url)
+        return True
+    return False
